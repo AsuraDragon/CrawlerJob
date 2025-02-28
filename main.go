1b27e35caeebd9cc08c19315ab0eb9071f86fa3c
Héctor Manuel Rasura Fragozo <rasurafragozo_@hotmail.com>
2025-02-28 01:07:08 -0700

    Fetch each Article HTML

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
 	"github.com/go-sql-driver/mysql"
+
 	// "github.com/securisec/go-keywords"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -26,12 +30,12 @@ func main() {
 	defer fmt.Println("crawler: (deferred) database connection closed")
 
 	// get a cursor to go through the table
-	
+
 	rows, err := db.Query("SELECT * FROM web_article")
-    if err != nil {
+	if err != nil {
 		fmt.Println("crawler: query with SELECT failed")
 		os.Exit(-1)
-    }
+	}
 
 	defer rows.Close()
 	defer println("crawler: (deferred) database row cursor closed")
@@ -40,7 +44,7 @@ func main() {
 	for rows.Next() {
 
 		fmt.Println("------------------------------------------")
-		
+
 		var article Article
 		if err := rows.Scan(&article.Id, &article.Title, &article.Url, &article.Keywords); err != nil {
 			fmt.Println("crawler: parsing article from rows failed, skipping")
@@ -52,12 +56,20 @@ func main() {
 
 		// process each article here
 
+		// get the HTML contents
+		htmlContent, err := getHtml(article.Url)
+		if err != nil {
+			fmt.Println("crawler: HTML content retrieval failed, skipping")
+			continue
+		}
+
+		fmt.Printf("HTML: %v\n\n", htmlContent[:1000])
 	}
 
 	fmt.Println("------------------------------------------")
 }
 
-func connectDB () (*sql.DB, error) {
+func connectDB() (*sql.DB, error) {
 
 	var db *sql.DB
 
@@ -73,29 +85,44 @@ func connectDB () (*sql.DB, error) {
 		return nil, err
 	}
 
-	cfg := mysql.Config {
-        User:   user,
-        Passwd: password,
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "it_support",
-    }
+	cfg := mysql.Config{
+		User:   user,
+		Passwd: password,
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "it_support",
+	}
 
 	// connect to the database
 	var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
-    if err != nil {
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
 		fmt.Printf("crawler: connectDB(): failed to connect to database %v\n", cfg.DBName)
-        return nil, err
-    }
+		return nil, err
+	}
 
 	// we can only be sure by pinging the database
-    pingErr := db.Ping()
-    if pingErr != nil {
+	pingErr := db.Ping()
+	if pingErr != nil {
 		fmt.Printf("crawler: connectDB(): failed to ping database %v (check log in credentials)\n", cfg.DBName)
-        return nil, pingErr
-    }
+		return nil, pingErr
+	}
 
-    fmt.Printf("crawler: connectDB(): successfully connected to database %v\n", cfg.DBName)
+	fmt.Printf("crawler: connectDB(): successfully connected to database %v\n", cfg.DBName)
 	return db, nil
+}
+
+func getHtml(url string) (string, error) {
+
+	res, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("crawler: GetHTML() cannot connect to %v", url)
+	}
+	content, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return "", fmt.Errorf("crawler: GetHtml() cannot close the responder")
+	}
+
+	return string(content), nil
 }
