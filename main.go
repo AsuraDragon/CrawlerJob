dd94cae933ecb671b79c424e0de8bf237676d1a2
Héctor Manuel Rasura Fragozo <rasurafragozo_@hotmail.com>
2025-03-02 01:42:21 -0700

    Run process as a Daemon to crawl periodically

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"fmt"
 	"html"
 	"regexp"
-	"strings"
+	"sort"
+	"time"
 
 	"github.com/go-sql-driver/mysql"
+
+	"github.com/securisec/go-keywords"
 
 	"io"
 	"net/http"
 	"os"
-	"sort"
 
-	"github.com/securisec/go-keywords"
+	"strings"
 )
 
 func main() {
@@ -44,73 +46,31 @@ func main() {
 
 	defer rows.Close()
 	defer println("crawler: (deferred) database row cursor closed")
-	// Loop through rows, using Scan to assign column data to struct fields.
 
-	for rows.Next() {
-
-		fmt.Println("------------------------------------------")
-
-		var article Article
-		if err := rows.Scan(&article.Id, &article.Title, &article.Url, &article.Keywords); err != nil {
-			fmt.Println("crawler: parsing article from rows failed, skipping")
-			continue
-		}
-
-		fmt.Printf("crawler: Article ID: %v Title: %v\n", article.Id, article.Title)
-		fmt.Printf("crawler: URL: %v\n\n", article.Url)
-
-		// get the HTML contents
-		htmlContent, err := getHtml(article.Url)
-		if err != nil {
-			fmt.Println("crawler: HTML content retrieval failed, skipping")
-			continue
-		}
-
-		fmt.Printf("HTML: %v\n\n", htmlContent[:1000])
-
-		titleToUse := extractTitle(htmlContent)
-		if titleToUse != "" {
-			// update the title column
-			_, err = db.Exec("UPDATE web_article SET title = ? WHERE id = ?", titleToUse, article.Id)
-			if err != nil {
-				fmt.Printf("crawler: query to update the title failed for article id %v\n", article.Id)
-			} else {
-				fmt.Printf("crawler: query to update the title for article id %v was successful\n", article.Id)
-			}
-		}
-
-		// extract keywords
-		keywordsGenerated := extractKeywords((article.Title + " " + article.Keywords))
-		keywordsAsString := strings.Join(keywordsGenerated, " ")
-		if len(keywordsAsString) > 0 {
-			// update the title column
-			_, err = db.Exec("UPDATE web_article SET keywords = ? WHERE id = ?", keywordsAsString, article.Id)
-			if err != nil {
-				fmt.Printf("crawler: query to update the keywords failed for article id %v\n", article.Id)
-			} else {
-				fmt.Printf("crawler: query to update the keywords for article id %v was successful\n", article.Id)
-			}
-		}
+	for {
+		processRows(db)
+		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>> Sleeping for 10 seconds")
+		time.Sleep(10 * time.Second)
 	}
-
-	fmt.Println("------------------------------------------")
 }
 
 func connectDB() (*sql.DB, error) {
 
 	var db *sql.DB
 
-	user := os.Getenv("DBUSER")
-	if user == "" {
-		err := fmt.Errorf("crawler: connectDB(): environmental variable DBUSER not set")
-		return nil, err
-	}
+	// user := os.Getenv("DBUSER")
+	// if user == "" {
+	// 	err := fmt.Errorf("crawler: connectDB(): environmental variable DBUSER not set")
+	// 	return nil, err
+	// }
 
-	password := os.Getenv("DBPASSWORD")
-	if password == "" {
-		err := fmt.Errorf("crawler: connectDB(): environmental variable DBPASSWORD not set")
-		return nil, err
-	}
+	// password := os.Getenv("DBPASSWORD")
+	// if password == "" {
+	// 	err := fmt.Errorf("crawler: connectDB(): environmental variable DBPASSWORD not set")
+	// 	return nil, err
+	// }
+	user := "ituser"
+	password := "ituser"
 
 	cfg := mysql.Config{
 		User:   user,
@@ -221,4 +181,72 @@ func extractKeywords(content string) []string {
 	fmt.Printf("crawler: extractKeywords() generated keywords: %v\n", keywordList)
 
 	return keywordList
+}
+
+func processRows(db *sql.DB) {
+
+	// get a cursor to go through the table
+
+	rows, err := db.Query("SELECT * FROM web_article")
+	if err != nil {
+		fmt.Println("crawler: query with SELECT failed")
+		os.Exit(-1)
+	}
+
+	defer rows.Close()
+	defer println("crawler: (deferred) database row cursor closed")
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+
+	for rows.Next() {
+
+		fmt.Println("------------------------------------------")
+
+		var article Article
+		if err := rows.Scan(&article.Id, &article.Title, &article.Url, &article.Keywords); err != nil {
+			fmt.Println("crawler: parsing article from rows failed, skipping")
+			continue
+		}
+
+		fmt.Printf("crawler: Article ID: %v Title: %v\n", article.Id, article.Title)
+		fmt.Printf("crawler: URL: %v\n\n", article.Url)
+
+		// process each article here
+
+		// get the HTML contents
+		htmlContent, err := getHtml(article.Url)
+		if err != nil {
+			fmt.Println("crawler: HTML content retrieval failed, skipping")
+			continue
+		}
+
+		fmt.Printf("HTML: %v\n\n", htmlContent[:1000])
+
+		// extract title
+		titleToUse := extractTitle(htmlContent)
+		if titleToUse != "" {
+			// update the title column
+			_, err = db.Exec("UPDATE web_article SET title = ? WHERE id = ?", titleToUse, article.Id)
+			if err != nil {
+				fmt.Printf("crawler: query to update the title failed for article id %v\n", article.Id)
+			} else {
+				fmt.Printf("crawler: query to update the title for article id %v was successful\n", article.Id)
+			}
+		}
+
+		// extract keywords
+		keywordsGenerated := extractKeywords((article.Title + " " + article.Keywords))
+		keywordsAsString := strings.Join(keywordsGenerated, " ")
+		if len(keywordsAsString) > 0 {
+			// update the title column
+			_, err = db.Exec("UPDATE web_article SET keywords = ? WHERE id = ?", keywordsAsString, article.Id)
+			if err != nil {
+				fmt.Printf("crawler: query to update the keywords failed for article id %v\n", article.Id)
+			} else {
+				fmt.Printf("crawler: query to update the keywords for article id %v was successful\n", article.Id)
+			}
+		}
+	}
+
+	fmt.Println("------------------------------------------")
 }
